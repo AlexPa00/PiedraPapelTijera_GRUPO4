3434c4fb8cb229a18628beee0fac1c8452e88029
Reina-Nerea <reinanerea85@gmail.com>
2022-09-25 01:32:40 +0200

    primera version de Home. Nerea Reina

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,22 @@
-export default function Home(){
+ import React from "react";
+ import imagen from "../img/piedra-papel-tijera.gif";
+ import "./estilos.css";
+
+ class Home extends React.Component{
+     render(){
+     return(
+        <main> 
+
+  
+    <h1 className= "titulo" >Piedra Papel Tijera</h1>
+    <img src=  {imagen} alt="" className="imagen"/>
+
+    <button role="button" className ="boton">Jugar</button>
+    <button role="button" className="boton">Desarrolladores</button>
     
-}
+     </main>
+      );
+   }
+ }
+
+ export default Home;
