271e7d3ea5251bb99ff0e3bc12e75a39ad1a909e
Casandra Reina <casandramailen.0010@gmail.com>
2022-09-24 17:02:32 -0300

    Cambios realizados: Se agrego nuevo contenido en los componentes Home y Desarrolladores , Casandra Reina

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Desarrolladores from './components/Desarrolladores';
+import Home from './components/Home';
+import desarrolladores from './desarrolladores.json';
+
+function App(){
+return(
+<div className="App">
+<header className="App-header">
+  {desarrolladores.map(des=>
+    <Desarrolladores
+     img={des.img}
+     nombre={des.nombre}
+     lu={des.lu}
+     descripcion={des.descripcion}
+    />
+   )}
+</header>
+</div>
+)
 }
 
 export default App;
