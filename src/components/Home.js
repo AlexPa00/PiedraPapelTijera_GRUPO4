333a83e9f6964dac0134516eb74636e74496aafb
Reina-Nerea <reinanerea85@gmail.com>
2022-09-25 23:17:56 +0200

    Cambios realizados en el modelo de home. Nerea Reina

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,7 @@
  import React from "react";
- import imagen from "../img/piedra-papel-tijera.gif";
+ import imagen from "../img/pa.png";
+ import imagen1 from "../img/pi.png";
+ import imagen2 from "../img/ti.png";
  import "./estilos.css";
 
  class Home extends React.Component{
@@ -8,10 +10,15 @@
      
         <main> 
       <div className="box-app"> 
-     <div className="box-title"> 
-    <h1 className= "titulo" >Piedra Papel Tijera</h1>
+      <div className="box-title"> 
+      <h1 className="titulo">Piedra Papel o Tijera</h1>
     </div>
+
+    <div className="contenedor"> 
     <img src=  {imagen} alt="" className="imagen"/>
+    <img src=  {imagen1} alt="" className="imagen"/>
+    <img src=  {imagen2} alt="" className="imagen"/>
+    </div>
 
     <button role="button" className ="boton">Jugar</button>
     <button role="button" className="boton">Desarrolladores</button>
