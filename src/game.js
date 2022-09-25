a84a0dab7d9157a825ababf2171606535a2acf48
AlexPa00 <nuve98@gmail.com>
2022-09-25 12:30:14 -0300

    en proceso

--- src/game.js
+++ src/game.js
@@ -3,7 +3,24 @@ import styled from "styled-components";
 import Token from "./token";
 
 const GameStyled = styled.div`
-display:flex;
+display:grid;
+grid-template-columns:130px 130px;
+justify-items:center;
+justify-content:center;
+grid-gap:50px;
+margin:2em 0;
+cursor:pointer;
+
+
+
+
+}
+
+& div:nth-of-type(3){
+ grid-column: span 2; 
+}
+
+
 `
 function Game() {
   return (
